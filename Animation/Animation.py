d525dbeaed2329099c290eb7e2140bc2154f80e6
Jaime Tuyuc <jaime.tuyuc@telus.com>
2024-05-12 13:33:30 -0600

    feat: stable version project

---
+++ Animation/Animation.py
@@ -0,0 +1,115 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.animation as animation
+from typing import Union
+# import matplotlib
+
+DURATION = 10
+NUM_FRAMES = 100
+GRAVITY = 9.81
+
+
+class ProjectileAnimation:
+    def __init__(self, v0: Union[int, float] = None, angle: Union[int, float] = None):
+        if not isinstance(v0, (int, float)) or not isinstance(angle, (int, float)):
+            raise TypeError("v0 and angle must be integers or floats")
+
+        self.g = GRAVITY
+        self.v0 = v0
+        self.angle = angle
+        self.num_frames = NUM_FRAMES
+        self.duration = DURATION
+
+        # Calculate the total time the object spends in the air
+        self.total_time_in_air = (2 * self.v0 * np.sin(np.radians(self.angle))) / self.g
+
+        # Adjust the number of frames and time range to match the total time in the air
+        self.t = np.linspace(0, self.total_time_in_air, self.num_frames)
+
+    def calculate_position(self):
+        vx0 = self.v0 * np.cos(np.radians(self.angle))
+        vy0 = self.v0 * np.sin(np.radians(self.angle))
+        xdata = vx0 * self.t
+        ydata = vy0 * self.t - 0.5 * self.g * self.t ** 2
+        y_max = (self.v0**2 * np.sin(np.radians(self.angle))**2) / 2 * self.g
+        print(y_max, 'y max ')
+
+        return xdata, ydata
+
+    def run_animation(self):
+        xdata, ydata = self.calculate_position()
+        max_height_index = np.argmax(ydata)
+        max_height_time = self.t[max_height_index]
+        max_height = ydata[max_height_index]
+
+        fig, ax = plt.subplots()
+
+        # Add background image
+        ax.imshow(plt.imread('images/bg3.jpeg'), extent=[0, xdata[-1] + 30, 0, max_height + 30])
+        canon_ball = plt.imread('images/canonball.png')
+        ballon = ax.imshow(canon_ball, extent=[0, 1, 0, 1], animated=True)
+
+        ax.set_xlabel('Eje (x) - distancia en metros')
+        ax.set_ylabel('Eje y - distancia en metros')
+        ax.set_xlim(0, xdata[-1] + 30)
+        ax.set_ylim(0, max_height + 30)
+        line, = ax.plot([], [], 'ro', animated=True)
+
+        # Add labels with additional information
+        label_x = 0.5 * (xdata[-1] + 30)
+        label_y = max_height + 50
+        ax.text(label_x, label_y,
+                f"Altura maxima alcanzada: {max_height:.2f} metros\nTiempo en alcanzar altura maxima: {max_height_time:.2f} segundos",
+                horizontalalignment='center', fontsize=12)
+
+        # Adjust the interval to match the total time in the air
+        interval = self.total_time_in_air / self.num_frames * 1000
+
+        def update_line(num, xdata, ydata, line):
+            line.set_data(xdata[:num], ydata[:num])
+            return line,
+
+        the_anim = animation.FuncAnimation(fig, update_line, frames=len(self.t),
+                                           fargs=(xdata, ydata, line),
+                                           interval=interval, blit=True)
+        plt.show()
+
+
+# Usage
+# projectile_anim = ProjectileAnimation(v0=200, angle=45)
+# projectile_anim.run_animation()
+
+    # def run_animation(self):
+    #     xdata, ydata = self.calculate_position()
+    #     max_height_index = np.argmax(ydata)
+    #     max_height_time = self.t[max_height_index]
+    #     max_height = ydata[max_height_index]
+    #
+    #     fig, ax = plt.subplots()
+    #
+    #     # Add background image
+    #     ax.imshow(plt.imread('images/bg3.jpeg'), extent=[0, xdata[-1] + 30, 0, max_height + 30])
+    #     cannon_ball = plt.imread('images/canonball1.png')
+    #
+    #     # Plot the cannonball image
+    #     ballon = ax.imshow(cannon_ball, extent=[0, 1, 0, 1], animated=True)
+    #
+    #     ax.set_xlabel('Eje (x) - distancia en metros')
+    #     ax.set_ylabel('Eje y - distancia en metros')
+    #     ax.set_xlim(0, xdata[-1] + 30)
+    #     ax.set_ylim(0, max_height + 30)
+    #
+    #     # Adjust the interval to match the total time in the air
+    #     interval = self.total_time_in_air / self.num_frames * 1000
+    #
+    #     def update_line(num, xdata, ydata, ballon):
+    #         ballon.set_extent([xdata[num] - 0.1, xdata[num] + 0.1, ydata[num] - 0.1,
+    #                            ydata[num] + 0.1])  # Update position of the cannonball
+    #         return ballon,
+    #
+    #     the_anim = animation.FuncAnimation(fig, update_line, frames=len(self.t),
+    #                                        fargs=(xdata, ydata, ballon),
+    #                                        interval=interval, blit=True)
+    #     plt.show()
+    # ! Changes here
---
+++ main.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import messagebox
+from UI import Labels
+from Animation import Animation
+import re
+
+
+def start_animation():
+    # * Get the velocity
+    value_velocity = input_velocity.get()
+    value_angle = angle_slider.get()
+
+    if value_velocity:
+        reset_button.configure(state='active')
+        reset_button.configure(state='active', cursor='arrow')
+        pattern = r"\."
+        casted_value = None
+        if re.search(pattern, value_velocity):
+            casted_value = float(value_velocity)
+        else:
+            casted_value = int(value_velocity)
+        draw_parabola = Animation.ProjectileAnimation(casted_value, value_angle)
+        draw_parabola.run_animation()
+    else:
+        messagebox.showwarning("Alerta", "Ingrese la velocidad inicial")
+
+
+def restart_animation():
+    input_velocity.delete(0, tk.END)
+    angle_slider.set(0)
+
+
+root = tk.Tk()
+root.geometry("400x400")
+root.title("Tiro parabolico")
+
+# @ Mostrar datos iniciales
+label_title = Labels.LabelsUi(text='Ingrese los datos minimos para calcular.', font=('Arial', 18),
+                              grid={'row': 0, 'column': 0, 'padx': 10, 'pady': 10, 'sticky': 'nsew'})
+label_title.pack_data()
+
+# @ label para la velocidad
+label_velocity_ms = Labels.LabelsUi(text='Ingrese la velocidad inicial',
+                                    grid={'row': 3, 'column': 0, 'padx': (10, 0), 'pady': 2, 'sticky': 'w'})
+label_velocity_ms.pack_data()
+
+label_velocity_ms = Labels.LabelsUi(text='m/s', grid={'row': 4, 'column': 1, 'pady': 5, 'padx': 5, 'sticky': 'w'})
+label_velocity_ms.pack_data()
+
+# @ Angle label
+angle_label = Labels.LabelsUi(text='Angulo de salida: 0', grid={'row': 7, 'column': 0, 'columnspan': 2, 'pady': 10})
+angle_label.pack_data()
+
+# @ Angle selector
+angle_slider = tk.Scale(root, from_=0, to=90, orient="horizontal",
+                        command=lambda value: angle_label.update_text(f"Angulo: {value} ° grados"))
+angle_slider.grid(row=6, column=0, padx=2, pady=2, columnspan=2)
+
+# Text input for velocity
+input_velocity = tk.Entry(root)
+input_velocity.grid(row=4, column=0, pady=0, padx=(10, 0), sticky='nsew')
+
+# Button to start animation
+start_button = tk.Button(root, text="CALCULAR", command=start_animation, width=15, height=2, foreground="green",
+                         font=("Arial", 14, 'bold'))
+start_button.grid(row=10, column=0, pady=(100, 0), padx=10, sticky='e', columnspan=2)
+# start_button.configure(state='disabled', cursor='circle')
+
+# @ Button to cancel or reset the animation
+reset_button = tk.Button(root, text="RESETEAR", width=15, height=2, bg='white', foreground="red",
+                         font=("Arial", 14, "bold"), command=restart_animation)
+reset_button.grid(row=10, column=0, padx=10, pady=(100, 0), sticky='w', columnspan=2)
+reset_button.configure(state='disabled', cursor='circle')
+
+root.mainloop()
